112dff9ae65d62cfb6b1679ab4294c390ef6984b
annisarachma <saa.annisa15@gmail.com>
2023-12-15 11:10:39 +0700

    feat: add get all /employees endpoint

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -2,74 +2,82 @@ const { Branch, Employee, Position } = require("../models");
 
 class EmployeeController {
   static async getAllEmployees(req, res, next) {
-    try{
-        const products = await Product.findAll({
-            include: [
-                {   model: Category,
-                    attributes : {
-                        exclude : ['createdAt', 'updatedAt']
-                    }
-                }
-            ],
-            attributes : {
-                exclude : ['createdAt', 'updatedAt']
+    try {
+      const employees = await Employee.findAll({
+        include: [
+          {
+            model: Branch,
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
             },
-            order : [['id', 'desc']]
-        })
-        res.status(200).json(products)
-    }
-    catch(err) {
-        next(err)
+          },
+          {
+            model: Position,
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          },
+        ],
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+        order : [['endDate']]
+      });
+      res.status(200).json(employees);
+    } catch (err) {
+      next(err);
     }
   }
 
   static async getEmployeeById(req, res, next) {
     try {
-        const product = await Product.findOne({
-            where : {id : req.params.productId},
-            include: [
-                {   model: Image,
-                    where : {productId : req.params.productId },
-                    attributes : {
-                        exclude : ['createdAt', 'updatedAt']
-                    }
-                },
-                {   model: User,
-                    attributes : {
-                        exclude : ['createdAt', 'updatedAt', 'password', 'role', 'phoneNumber', 'address']
-                    }
-                },
-                {   model: Category,
-                    attributes : {
-                        exclude : ['createdAt', 'updatedAt']
-                    }
-                },
-
-            ],
-            attributes : {
-                exclude : ['createdAt', 'updatedAt']
+      const product = await Product.findOne({
+        where: { id: req.params.productId },
+        include: [
+          {
+            model: Image,
+            where: { productId: req.params.productId },
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
             },
-        })
-        if(!product) throw {name : "NotFound"} 
-        res.status(200).json(product)
+          },
+          {
+            model: User,
+            attributes: {
+              exclude: [
+                "createdAt",
+                "updatedAt",
+                "password",
+                "role",
+                "phoneNumber",
+                "address",
+              ],
+            },
+          },
+          {
+            model: Category,
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          },
+        ],
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      });
+      if (!product) throw { name: "NotFound" };
+      res.status(200).json(product);
     } catch (err) {
-        console.log(err)
+      console.log(err);
       next(err);
     }
   }
 
-  static async postEmployee(req, res, next) {
+  static async postEmployee(req, res, next) {}
 
-  }
+  static async editEmployeeById(req, res, next) {}
 
-  static async editEmployeeById(req, res, next) {
-    
-  }
-
-  static async deleteEmployeeById(req, res, next) {
-    
-  }
-
+  static async deleteEmployeeById(req, res, next) {}
 }
 
 module.exports = EmployeeController;
